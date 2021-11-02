23b5b7371929a4a8ffec260051c21e19cc2cda6e
Quentin <quentinrougemont@orange.fr>
2021-11-02 16:09:53 +0100

    add hierfstat

---
+++ 00-scripts/diversity/00.hierfstats.R
@@ -0,0 +1,35 @@
+library(hierfstat)
+
+#read data
+data <-read.table("hierfstat.data.txt", header=T ) #, sep="\t")
+
+stats <- basic.stats(data, diploid=TRUE)
+write.table(stats$perloc,"hierfstat.diversity.index.",quote=F,row.names=T,col.names=T)
+write.table(stats$Ho, "hierfstat.ho",quote=F,row.names=T,col.names = T)
+write.table(stats$Hs, "hierfstat.hs",quote=F,row.names=T,col.names = T)
+write.table(stats$Fis,"hierfstats.fis",quote=F, row.names=T, col.names=T)
+write.table(stats$overall,"hierfstats.overall",quote=F,row.names=T,col.names=T)
+
+
+beta1 <- betas(data, nboot=100)
+write.table(t(rbind(beta1$betaiovl,beta1$ci)),"beta_iovl_ci", col.names=T, row.names=T) 
+write.table(t(rbind(beta1$betaiovl,beta1$ci)),"beta_iovl_ci2", col.names=c("beta","2.5%","97.5%"), row.names=T)
+
+fis <- boot.ppfis(data, nboot = 1000) 
+
+write.table(b$fis.ci, "hierfstat.confidence.interval.fis.txt",
+    quote = F, row.names = T, col.names = T)
+
+
+ho <- read.table("hierfstat.ho")
+hs <- read.table("hierfstat.hs")
+
+ho2 <- data.frame(colMeans(ho[,-1], na.rm=T))
+colnames(ho2) <- "ho"
+
+hs2 <- data.frame(colMeans(hs[,-1], na.rm=T))
+colnames(hs2) <- "hs"
+
+write.table(ho2,"mean_Ho_hierfstat.txt",quote=F)
+write.table(hs2,"mean_Hs_hierfstat.txt",quote=F)
+
